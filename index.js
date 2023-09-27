6a4c3a4b8f1e0ead9d26237a04a1bbd27f31508c
Li Pon <mirnatul@gmail.com>
2023-09-27 19:09:48 +0600

    server ready for news section and single news

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.get("/", (req, res) => {
 
 // MongoDB all code HERE================
 
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.7rh25i5.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -49,6 +49,13 @@ async function run() {
     app.get("/movieNewsList", async (req, res) => {
       const movieNewsList = await movieNewsCollection.find().toArray();
       res.send(movieNewsList);
+    });
+
+    app.get("/movieNewsList/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await movieNewsCollection.findOne(query);
+      res.send(result)
     });
 
     // Send a ping to confirm a successful connection
