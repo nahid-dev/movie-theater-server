c722aee82dc372c979d491d5d49534fd6e578c49
nahid-dev <nahidv232@gmail.com>
2024-02-02 23:50:25 +0600

    seat booking page api done

--- index.js
+++ index.js
@@ -100,6 +100,14 @@ async function run() {
       res.send({ result, result2 });
     });
 
+    // ALL MOVIE BOOKING API'S HERE
+    app.get("/bookingSeats/:id", async (req, res) => {
+      const id = req.params;
+      const query = { _id: new ObjectId(id) };
+      const result = await showingMovies.findOne(query);
+      res.send(result);
+    });
+
     // ALL MOVIE NEWS API
     app.get("/movieNewsList", async (req, res) => {
       const movieNewsList = await movieNewsCollection.find().toArray();
